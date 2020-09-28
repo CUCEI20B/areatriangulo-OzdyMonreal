9b08bfaef32eba33f402bd1b8cf247b80d062ee0
Edgar Omar Monreal <edgar.monreal7686@alumnos.udg.mx>
2020-09-28 16:40:23 -0500

    Prueba 01

--- main.cpp
+++ main.cpp
@@ -3,7 +3,13 @@
 using namespace std;
 
 int main() {
-    cout << "Hola Mundo!" << endl; // eliminar esta línea
+  unsigned int base;
+  unsigned int altura;
 
-    return 0;
+  cin >> base;
+  cin >> altura;
+
+  cout << (base * altura) / 2 << endl;
+
+  return 0;
 }
